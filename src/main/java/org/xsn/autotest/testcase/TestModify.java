829580a70a3cb0a9d939262183602d6a92a559de
liu <afan6203@126.com>
2019-07-10 19:10:03 +0800

    怎么处理webdriver呢

---
+++ src/main/java/org/xsn/autotest/testcase/TestModify.java
@@ -0,0 +1,11 @@
+package org.xsn.autotest.testcase;
+
+import org.testng.annotations.Test;
+
+public class TestModify {
+	
+	@Test(groups = {"tp"})
+	public void testModifySuccess() {
+//		ModifyPasswordPage page = new ModifyPasswordPage(webDriver);
+	}
+}
