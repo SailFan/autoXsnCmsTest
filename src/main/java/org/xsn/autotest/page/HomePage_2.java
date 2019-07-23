3077ce245f26885e4d145aa09b38b80d72ba115c
liu <afan6203@126.com>
2019-07-23 16:56:26 +0800

    問題太多了, 太不穩定了

--- src/main/java/org/xsn/autotest/page/HomePage_2.java
+++ src/main/java/org/xsn/autotest/page/HomePage_2.java
@@ -11,21 +11,24 @@ import org.xsn.autotest.common.PublicMethod;
 public class HomePage_2 extends PublicMethod {
 	private final static Logger logger = Logger.getLogger(HomePage_2.class);
 	WebDriver brower = null;
-	By loginOut = By.partialLinkText("退出");
-	By currentUser = By.cssSelector(".header-right li:nth-of-type(4)");
-	By modifyPasswordButton = By.partialLinkText("修改密码");
-	By Lis = By.cssSelector(".el-menu-vertical-demo.el-menu>li");
-
+	public By loginOut = By.partialLinkText("退出");
+	public By currentUser = By.cssSelector(".header-right li:nth-of-type(4)");
+	public By modifyPasswordButton = By.partialLinkText("修改密码");
+	public By Lis = By.cssSelector(".el-menu-vertical-demo.el-menu>li");
+	//待审核文章li
+	public By dSHWZLi= By.xpath("/html/body/div[1]/section/section/aside/ul/li[1]/ul/li/ul/li[1]");
+	//内容列表
+	public By nRLBLi = By.xpath("/html/body/div[1]/section/section/aside/ul/li[1]/ul/li/ul/li[2]");
+	
 //	public HomePage_2() {
 //		brower = Util.openBrower("chrome", null);
 //		super.webDriver = brower;
 //	}
 	
-	public WebElement getNavigation(){
+	//获取全部左侧导航
+	public List<WebElement> getNavigation(){
 		List<WebElement> list = webDriver.findElements(Lis);
-		for (WebElement webElement : list) {
-			webElement.click();
-		}
-		return null;
+		return list;
 	}
+	
 }
--- src/main/java/org/xsn/autotest/testcase/TestHomePage.java
+++ src/main/java/org/xsn/autotest/testcase/TestHomePage.java
@@ -1,45 +1,106 @@
 package org.xsn.autotest.testcase;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import org.xsn.autotest.common.Util;
 import org.xsn.autotest.page.HomePage;
+import org.xsn.autotest.page.HomePage_2;
 import org.xsn.autotest.page.LoginPage;
 
 public class TestHomePage {
+	private final static Logger logger = Logger.getLogger(TestHomePage.class);
 	private WebDriver brower;
 	private LoginPage loginPage;
 	private HomePage homePage;
+	HomePage_2 page_2;
 
-	@BeforeClass
+	@BeforeClass(groups = { "check" })
 	public void setUp() {
-		brower = Util.openBrower("chrome",null);
+		brower = Util.openBrower("chrome", null);
 		loginPage = new LoginPage(brower);
 		homePage = new HomePage(brower);
+		page_2 = new HomePage_2();
+		loginPage.loginToXsn("admin", "123456");
 	}
 
-	@Test
-	public void testOpenModifyPageSuccess() {
+	@AfterClass(groups = { "check" })
+	public void afterClass() {
+//		brower.close();
+	}
+
+	@BeforeMethod(groups = { "correct" })
+	public void beforeMethod() {
+		brower = Util.openBrower("chrome", null);
+		loginPage = new LoginPage(brower);
+		homePage = new HomePage(brower);
 		loginPage.loginToXsn("admin", "123456");
+	}
+
+	@AfterMethod(groups = { "correct" })
+	public void afterMethod() {
+		brower.close();
+	}
+
+	@Test(groups = { "correct" })
+	public void testOpenModifyPageSuccess() {
 		homePage.myDDwait(10).until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("修改密码")));
 		homePage.clickModifyButton();
 		homePage.myDDwait(10).until(ExpectedConditions.titleIs("修改密码"));
 		Assert.assertEquals("修改密码", homePage.currentPageTitle());
+		Reporter.log("测试打开修改密码页面");
 	}
 
-	@Test
+	@Test(groups = { "correct" })
 	public void testLoginout() {
-//		loginPage.loginToXsn("admin", "123456");
 		homePage.myDDwait(10).until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("退出")));
 		homePage.clickLogoutButton();
 		homePage.myDDwait(10).until(ExpectedConditions.visibilityOf(homePage.confireLoginButton));
 		homePage.confireLogout();
 		Assert.assertEquals(homePage.currentPageUrl(), "http://test.admin.vocy.cn/#/login");
+		Reporter.log("测试退出按键");
 	}
+
+	@Test(groups = { "check" })
+	public void testOpenNavigation() {
+		logger.debug("打开左侧导航栏");
+		brower.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		List<WebElement> list = page_2.getNavigation();
+		for (WebElement webElement : list) {
+			webElement.click();
+		}
+		Reporter.log("测试打开左侧导航");
+	}
+	
+	
+	@Test(dependsOnMethods = "testOpenNavigation", groups = {"check"})
+	@Ignore
+	public void testCloseNavigation() {
+		logger.debug("全部关闭左侧导航栏");
+		brower.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		List<WebElement> list = page_2.getNavigation();
+		for (WebElement webElement : list) {
+			webElement.click();
+		}
+//		WebElement element = brower.findElement(page_2.dSHWZLi);
+		Reporter.log("测试关闭左侧导航");
+	}
+	
+	
 
 }
