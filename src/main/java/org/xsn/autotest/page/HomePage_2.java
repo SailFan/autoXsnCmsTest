0a75344dec86d34b8a10334bbf83fd1134f9028f
liu <afan6203@126.com>
2019-08-23 12:26:25 +0800

    修改homepage2页面,新增查找元素的方法

--- src/main/java/org/xsn/autotest/page/HomePage_2.java
+++ src/main/java/org/xsn/autotest/page/HomePage_2.java
@@ -10,7 +10,12 @@ import org.xsn.autotest.common.PublicMethod;
 
 public class HomePage_2 extends PublicMethod {
 	private final static Logger logger = Logger.getLogger(HomePage_2.class);
-	WebDriver brower = null;
+
+	public HomePage_2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 //	退出按键
 	public By loginOut = By.partialLinkText("退出");
 //	当前用户
@@ -40,7 +45,7 @@ public class HomePage_2 extends PublicMethod {
 //	小记者推荐
 	public By youngReporter = By.xpath("//aside/ul/li[3]/ul/li/ul/li[1]");
 //	待审核新建学校列表
-	public By SchoolToBeAudited = By.xpath("//aside/ul/li[3]/ul/li/ul/li[1]");
+	public By schoolToBeAudited = By.xpath("//aside/ul/li[3]/ul/li/ul/li[1]");
 //	学校列表
 	public By schoolList = By.xpath("//aside/ul/li[3]/ul/li/ul/li[2]");
 //	公众号列表
@@ -58,15 +63,14 @@ public class HomePage_2 extends PublicMethod {
 //	站点管理
 	public By siteManage = By.xpath("//aside/ul/li[8]/ul/li/ul/li[1]");
 //	内容频道管理
-	public By ContentOfTheChannelManage = By.xpath("//aside/ul/li[8]/ul/li/ul/li[2]");
+	public By contentOfTheChannelManage = By.xpath("//aside/ul/li[8]/ul/li/ul/li[2]");
 //	系统内容分类
-	public By SystemContentClassification = By.xpath("//aside/ul/li[8]/ul/li/ul/li[3]");
+	public By systemContentClassification = By.xpath("//aside/ul/li[8]/ul/li/ul/li[3]");
 //	角色管理
 	public By roleManage = By.xpath("//aside/ul/li[8]/ul/li/ul/li[4]");
 //	用户管理
 	public By userManage = By.xpath("//aside/ul/li[8]/ul/li/ul/li[5]");
-	
-	
+
 	// 获取全部二级导航菜单
 	public List<WebElement> getNavigation() {
 		List<WebElement> elements = webDriver.findElements(navSecond);
@@ -77,5 +81,137 @@ public class HomePage_2 extends PublicMethod {
 	public List<WebElement> getFirstStageNav() {
 		List<WebElement> list = webDriver.findElements(this.navFirst);
 		return list;
+	}
+
+	// 查找待审核列表
+	public WebElement getToAuditList() {
+		WebElement element = super.webDriver.findElement(this.toAuditList);
+		return element;
+	}
+
+//	查找内容列表
+	public WebElement getContentList() {
+		WebElement element = super.webDriver.findElement(this.contentList);
+		return element;
+	}
+
+//	内容推荐
+	public WebElement getContentRecommendation() {
+		WebElement element = super.webDriver.findElement(this.contentRecommendation);
+		return element;
+	}
+
+//	查找轮播图
+	public WebElement getSlideShow() {
+		WebElement element = super.webDriver.findElement(this.slideShow);
+		return element;
+	}
+
+//	热文章
+	public WebElement getHotArtcle() {
+		WebElement element = super.webDriver.findElement(this.hotArtcle);
+		return element;
+	}
+
+//	热话题管理
+	public WebElement getHotTopic() {
+		WebElement element = super.webDriver.findElement(this.hotTopic);
+		return element;
+	}
+
+//	查找热校区
+	public WebElement getHotSchool() {
+		WebElement element = super.webDriver.findElement(this.hotSchool);
+		return element;
+	}
+
+//	查找名师工作室推荐列表
+	public WebElement getMasterStudio() {
+		WebElement element = super.webDriver.findElement(this.masterStudio);
+		return element;
+	}
+
+//	查找小记者
+	public WebElement getYoungReporter() {
+		WebElement element = super.webDriver.findElement(this.youngReporter);
+		return element;
+	}
+
+//	查找待审核列表学校列表
+	public WebElement getSchoolToBeAudited() {
+		WebElement element = super.webDriver.findElement(this.schoolToBeAudited);
+		return element;
+	}
+
+//	查找学校列表
+	public WebElement getSchoolList() {
+		WebElement element = super.webDriver.findElement(this.schoolList);
+		return element;
+	}
+
+//	查找公众号列表
+	public WebElement getOfficialAccounts() {
+		WebElement element = super.webDriver.findElement(this.officialAccounts);
+		return element;
+	}
+
+//	名师工作室
+	public WebElement getMasterStudioList() {
+		WebElement element = super.webDriver.findElement(this.masterStudioList);
+		return element;
+	}
+
+//	查找话题列表
+	public WebElement getTopicList() {
+		WebElement element = super.webDriver.findElement(this.topicList);
+		return element;
+	}
+
+//	专题列表
+	public WebElement getSpecialList() {
+		WebElement element = super.webDriver.findElement(this.specialList);
+		return element;
+	}
+
+//		小程序
+	public WebElement getMiniProgram() {
+		WebElement element = super.webDriver.findElement(this.miniProgram);
+		return element;
+	}
+
+//	获取专辑列表
+	public WebElement getAlbumList() {
+		WebElement element = super.webDriver.findElement(this.albumList);
+		return element;
+	}
+
+//	站点管理
+	public WebElement getSiteManage() {
+		WebElement element = super.webDriver.findElement(this.siteManage);
+		return element;
+	}
+
+//	内容频道管理
+	public WebElement getContentOfTheChannelManage() {
+		WebElement element = super.webDriver.findElement(this.contentOfTheChannelManage);
+		return element;
+	}
+
+//	系统内容分类
+	public WebElement getSystemContentClassification() {
+		WebElement element = super.webDriver.findElement(this.systemContentClassification);
+		return element;
+	}
+
+//	查找角色管理列表
+	public WebElement getRoleManage() {
+		WebElement element = super.webDriver.findElement(this.roleManage);
+		return element;
+	}
+
+//	用户管理
+	public WebElement getUserManage() {
+		WebElement element = super.webDriver.findElement(this.userManage);
+		return element;
 	}
 }
